36ca6e3e67ba5e62dff86b735490a7616bcc29f4
Etoh <realsusmit18@gmail.com>
2024-10-07 13:00:02 +0530

    Final all updated files

--- run_model.py
+++ run_model.py
@@ -13,7 +13,6 @@ def  run_model(modelPath):
 
     columns=['PM2.5','PM10','NO','NO2','NOx','NH3','CO','SO2','O3','Benzene','Toluene','Xylene']
     
-    # future_X = pd.DataFrame({'PM2.5' : [25.58],'PM10' : [91.73],'NO': [17.5],'NO2': [39.32], 'NOx': [20.13], 'NH3': [5.7], 'CO': [0.02], 'SO2': [10.14], 'O3': [2.3], 'Benzene': [5.97], 'Toluene': [15.23], 'Xylene': [4.13]})
     data = pd.DataFrame(userInput(columns=columns))
 
     future_AQI = model.predict(data)
